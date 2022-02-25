848e19a6a6814d2d2181a09bab8ea9c8ce8454f2
YtaloCampos <ytalo.campos@tallos.com.br>
2022-02-25 14:11:59 -0300

    add spotify public profile feature

--- src/domain/features/spotify-public-profile.ts
+++ src/domain/features/spotify-public-profile.ts
@@ -1,11 +1,18 @@
+import { SpotifyProfile } from "../models/spotify-profile";
+
+type Params = SpotifyPublicProfile.Params;
+type Result = SpotifyPublicProfile.Result;
+
 export interface SpotifyPublicProfile {
-  execute: () => undefined;
+  execute: (params: Params) => Result;
 }
 
 export namespace SpotifyPublicProfile {
-  export interface Params {
+  export type Params = {
     clientId: string;
     clientSecret: string;
     username: string;
-  }
+  };
+
+  export type Result = SpotifyProfile;
 }
---
+++ src/domain/models/spotify-profile.ts
@@ -0,0 +1,7 @@
+type ExternalUrls = { spotify: string };
+
+export type SpotifyProfile = {
+  display_name: string;
+  external_urls: ExternalUrls;
+  id: string;
+};
