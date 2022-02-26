dd16fd99f5bef93e3693b9ae4e9ff11f3d2058e6
YtaloCampos <ytalo.campos@tallos.com.br>
2022-02-26 17:06:35 -0300

    refact: mocking tests with jest mock extended

--- tests/data/services/spotify-public-profile.spec.ts
+++ tests/data/services/spotify-public-profile.spec.ts
@@ -1,39 +1,27 @@
 import { LoadSpotifyUser } from "@/data/interfaces";
 import { SpotifyPublicProfileService } from "@/data/services";
-
-class LoadSpotifyUserSpy implements LoadSpotifyUser {
-  public username?: string;
-  public result: undefined;
-
-  async perform(
-    params: LoadSpotifyUser.Params
-  ): Promise<LoadSpotifyUser.Result> {
-    this.username = params.username;
-    return this.result;
-  }
-}
+import { mock } from "jest-mock-extended";
 
 describe("SpotifyPublicProfileService", () => {
   it("should to call spotify public profile with correct params", async () => {
-    const loadSpotifyUserApi = new LoadSpotifyUserSpy();
-    const sut = new SpotifyPublicProfileService(loadSpotifyUserApi);
-
+    const loadSpotifyUser = mock<LoadSpotifyUser>();
+    const sut = new SpotifyPublicProfileService(loadSpotifyUser);
     await sut.perform({
       username: "any_username",
     });
-
-    expect(loadSpotifyUserApi.username).toBe("any_username");
+    expect(loadSpotifyUser.perform).toHaveBeenCalledWith({
+      username: "any_username",
+    });
+    expect(loadSpotifyUser.perform).toHaveBeenCalledTimes(1);
   });
 
-  it("should return undefined when loadSpotifyUserApi returns undefined", async () => {
-    const loadSpotifyUserApi = new LoadSpotifyUserSpy();
-    loadSpotifyUserApi.result = undefined;
-    const sut = new SpotifyPublicProfileService(loadSpotifyUserApi);
-
+  it("should return undefined when loadSpotifyUser returns undefined", async () => {
+    const loadSpotifyUser = mock<LoadSpotifyUser>();
+    loadSpotifyUser.perform.mockResolvedValueOnce(undefined);
+    const sut = new SpotifyPublicProfileService(loadSpotifyUser);
     const result = await sut.perform({
       username: "any_username",
     });
-
     expect(result).toBe(undefined);
   });
 });
